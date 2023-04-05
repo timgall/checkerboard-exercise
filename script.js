bac9c0ab82ec5bd81be93ecb5fcf71f1962e0c8d
timgall <119544130+timgall@users.noreply.github.com>
2023-04-05 17:47:04 -0500

    flash

--- script.js
+++ script.js
@@ -5,25 +5,40 @@ container.style.width = "800px";
 container.style.height = "800px";
 document.body.append(container)
 function randomColor(){
-  
   return Math.floor(Math.random()*255)
 }
 console.log(randomColor())
 
-for(let i=0; i<8; i++){
+function randomTransp(){
+  return (Math.round(Math.random()*99)+1)+'%'
+}
+console.log(randomTransp())
+  for(let i=0; i<8; i++){
   for(let j = 0; j<8; j++){
     var tile = document.createElement("div");
     tile.style.width = "12.5%";
     tile.style.width = "12.5%";
-  
+
     if(i%2===j%2){
       tile.style.background=`rgb(${randomColor()},${randomColor()},${randomColor()})`
     }else{
         tile.style.background=`rgb(${randomColor()},${randomColor()},${randomColor()})`
-    }
-    container.appendChild(tile)
-}
-}
+    }  
 
+  container.appendChild(tile)
+  }
+  }
+  let intervalId;
+// define a function that changes the color of each tile to a new random color
+function changeTileColors() {
+  const tiles = container.querySelectorAll("div");
+  tiles.forEach((tile) => {
+    tile.style.background = `rgb(${randomColor()},${randomColor()},${randomColor()})`;
+  });
+}
+// start the interval and store the ID in the intervalId variable
+intervalId = setInterval(changeTileColors, 2000);
+Animation
 
+// animation-timing-function='ease-in-out'
 //instead of j do const row = Math.floor(i/8) and replace it where J is
